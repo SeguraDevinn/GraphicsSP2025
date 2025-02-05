3c7846b01ea2fdb85df0faedf48fca87978497fd
devinnsegura <devinn.segura@calbaptist.edu>
2025-02-05 09:50:05 -0800

    Class and Teapot object added

---
+++ src/RenderTask.java
@@ -0,0 +1,6 @@
+public class RenderTask implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/Scene.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public class Scene {
+    ArrayList<Vector3> vertices = new ArrayList<>();
+    ArrayList<Vector3> faces = new ArrayList<>();
+}
