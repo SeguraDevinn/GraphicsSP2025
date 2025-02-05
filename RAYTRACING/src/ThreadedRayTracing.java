66592916f84851127b60f51257c45fb04350ab3c
devinnsegura <devinn.segura@calbaptist.edu>
2025-02-05 10:04:12 -0800

    reformatted so that we can hold multiple projects here

---
+++ RAYTRACING/src/ThreadedRayTracing.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+
+public class ThreadedRayTracing {
+    public static void main(String[] args) throws IOException {
+        Scene scene = OBJParser.parse("teapot.obj");
+
+    }
+}
+
+
