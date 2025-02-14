ce9760574ecaacec570a2bbecf923470d141cf89
Joshua Rivera <joshua.rivera1@calbaptist.edu>
2025-02-13 23:46:04 -0800

    Face and Vector2

--- TextureMapping/src/Scene.java
+++ TextureMapping/src/Scene.java
@@ -2,5 +2,11 @@
 This will be done by franchesco
  */
 
-public class Scene {
+import java.util.ArrayList;
+
+public class Scene
+{
+    ArrayList<Vector3> vertices = new ArrayList<>();
+    ArrayList<Vector2> uvs = new ArrayList<>();
+    ArrayList<Face> faces = new ArrayList<>();
 }
--- TextureMapping/src/Vector2.java
+++ TextureMapping/src/Vector2.java
@@ -1,5 +1,12 @@
 /*
 This will be done by franchesco
  */
-public class Vector2 {
+public class Vector2
+{
+    double x, y;
+    Vector2(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
 }
