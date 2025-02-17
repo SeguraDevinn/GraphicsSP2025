52b1a6703106265815432a6cc4c9803bb1061abf
devinnsegura <devinn.segura@calbaptist.edu>
2025-02-17 09:44:50 -0800

    Setup complete

---
+++ CelShading/src/CelShading.java
@@ -0,0 +1,80 @@
+import java.io.IOException;
+
+public class CelShading {
+    //Devinn
+    public void run() {
+
+    }
+    //Josh
+    private void init() {
+
+    }
+    //Franchesco
+    private void loop() {
+
+    }
+    //Devinn
+    private void cleanup() {
+
+    }
+    //Josh
+    private void setPerspective(float fov, float aspectRatio, float near, float far) {
+
+    }
+    //Franchesco
+    private void gluLookAt(float eyeX, float eyeY, float eyeZ,
+                           float centerX, float centerY, float centerZ,
+                           float upX, float upY, float upZ)
+    {
+
+    }
+    //Devinn
+    private void setupCamera() {
+
+    }
+    //Josh
+    public void setupLighting() {
+
+    }
+    //Franchesco
+    public void setupMaterial() {
+
+    }
+    //Devinn
+    public void renderModel(Model model) {
+
+    }
+    //Josh
+    private float[] calculateFaceNormal(Model model, int[] face) {
+
+    }
+    //Franchesco
+    private float calculateDiffuseLight(float[] normal, float[] lightDir) {
+
+    }
+    //Devinn
+    private float quantizeLight(float intensity, int levels) {
+
+    }
+    //Josh
+    public void renderOutline(Model model) {
+
+    }
+    //Franchesco
+    public static void main(String[] args) {
+        new CelShading().run();
+    }
+
+    /*
+    This is the start of the classes and its functions
+     */
+    //Devinn
+    public static class Model{
+
+    }
+    //Josh
+    public static Model loadModel(String filename) throws IOException {
+
+    }
+
+}
--- TextureMapping/src/RayTracerTexture.java
+++ TextureMapping/src/RayTracerTexture.java
@@ -3,22 +3,19 @@
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import javax.imageio.ImageIO;
 
 public class RayTracerTexture
 {
     public static void main(String[] args) throws IOException
     {
-        Scene scene = OBJParser.parse("TextureMapping\\src\\uvSphere.obj");
+        Scene scene = OBJParser.parse("TextureMapping/src/uvSphere.obj");
         scene.spheres.add(new Sphere(new Vector3(0, 0, 0), 1.0));
-        Camera camera = new Camera(new Vector3(0, 0, 3), new Vector3(0, 0, 0), 90);
+        Camera camera = new Camera(new Vector3(0, 0, -3), new Vector3(0, 0, 0), 90);
 
         // Rotate for 30 degrees around the X-axis
         Matrix4x4 rotationMatrix = RotationUtil.glRotate(30, 1, 0, 0);
@@ -35,7 +32,7 @@ public class RayTracerTexture
         int width = 800;
         int height = 600;
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        BufferedImage texture = ImageIO.read(new File("TextureMapping\\src\\texture.png")); // Load texture image
+        BufferedImage texture = ImageIO.read(new File("TextureMapping/src/texture.png")); // Load texture image
 
         for (int y = 0; y < height; y++)
         {
@@ -55,40 +52,48 @@ public class RayTracerTexture
                 image.setRGB(x, y, color.getRGB());
             }
         }
-        ImageIO.write(image, "png", new File("TextureMapping\\src\\outputUvSphereModified.png"));
+        ImageIO.write(image, "png", new File("TextureMapping\\src\\output.png"));
 
-        exportOBJ(scene, "TextureMapping\\src\\outputSphere.obj");
+        exportSphere(scene, "TextureMapping\\src\\outputSphere.obj");
         System.out.println("OBJ file saved successfully.");
     }
 
-    public static void exportOBJ(Scene scene, String filePath) throws IOException
+    public static void exportSphere(Scene scene, String filePath) throws IOException
     {
-        File file = new File(filePath);
-        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+       File outputFile = new File(filePath);
+    try (BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile))) {
 
-        // Write vertices
-        for (Vector3 vertex : scene.vertices)
-        {
-            writer.write("v " + vertex.x + " " + vertex.y + " " + vertex.z + "\n");
+        // Write vertex positions
+        for (Vector3 point : scene.vertices) {
+            bw.write("v " + point.x + " " + point.y + " " + point.z);
+            bw.newLine();
         }
 
-        // Write UV coordinates
-        for (Vector2 uv : scene.uvs)
-        {
-            writer.write("vt " + uv.x + " " + uv.y + "\n");
+        // Write texture coordinates (UVs)
+        for (Vector2 texCoord : scene.uvs) {
+            bw.write("vt " + texCoord.x + " " + texCoord.y);
+            bw.newLine();
         }
 
-        // Write faces (assume triangles)
-        for (Face face : scene.faces)
-        {
-            writer.write("f " + (face.vertices[0] + 1) + "/" + (face.uvs[0] + 1) + " "
-                    + (face.vertices[1] + 1) + "/" + (face.uvs[1] + 1) + " "
-                    + (face.vertices[2] + 1) + "/" + (face.uvs[2] + 1) + "\n");
-        }
+        // Write faces (assuming each face is a triangle)
+        for (Face triangle : scene.faces) {
+            int vertIndex1 = triangle.vertices[0] + 1;
+            int vertIndex2 = triangle.vertices[1] + 1;
+            int vertIndex3 = triangle.vertices[2] + 1;
 
-        writer.close();
+            int uvIndex1 = triangle.uvs[0] + 1;
+            int uvIndex2 = triangle.uvs[1] + 1;
+            int uvIndex3 = triangle.uvs[2] + 1;
+
+            String faceLine = String.format("f %d/%d %d/%d %d/%d",
+                    vertIndex1, uvIndex1,
+                    vertIndex2, uvIndex2,
+                    vertIndex3, uvIndex3);
+            bw.write(faceLine);
+            bw.newLine();
+        }
     }
-
+}
     private static Color sampleTexture(BufferedImage texture, Vector2 uv)
     {
         int texWidth = texture.getWidth();
@@ -106,51 +111,44 @@ public class RayTracerTexture
         return new Color(texture.getRGB(texX, texY));
     }
 
-    private static Vector2 computeSphericalUV(Vector3 point, Sphere sphere)
-    {
-        Vector3 normalizedPoint = point.subtract(sphere.center).normalize();
+    private static Vector2 SphereUV(Vector3 point, Sphere sphere) {
 
-        // create variables for latitude and longitude and normalize them (u,v)
-        double theta = Math.atan2(normalizedPoint.z, normalizedPoint.x);
-        double phi = Math.acos(normalizedPoint.y);
-
-        double u = (theta + Math.PI) / (2 * Math.PI);
-        double v = phi / Math.PI;
-
-        return new Vector2(u, v);
-    }
+    Vector3 relativePos = point.subtract(sphere.center).normalize();
 
 
-    private static Color traceRay(Ray ray, Scene scene, BufferedImage texture)
-    {
-        Intersection closestIntersection = null;
-        Sphere closestSphere = null;
+    double longitude = Math.atan2(relativePos.z, relativePos.x);
+    double latitude = Math.acos(relativePos.y);
 
-        for (Sphere sphere : scene.spheres)
-        {
-            Intersection intersection = RayTracer.intersectRaySphere(ray, sphere);
-            if (intersection.hit && (closestIntersection == null || intersection.distance < closestIntersection.distance))
-            {
-                closestIntersection = intersection;
-                closestSphere = sphere;
-            }
-        }
 
-        if (closestIntersection != null)
-        {
-            Vector2 sphericalUV = computeSphericalUV(closestIntersection.point, closestSphere);
-            Color color = sampleTexture(texture, sphericalUV);
+    double uCoord = (longitude + Math.PI) / (2 * Math.PI);
+    double vCoord = latitude / Math.PI;
 
-            // Debugging Output
-            System.out.println("Hit: " + closestIntersection.point + " UV: " + sphericalUV.x + ", " + sphericalUV.y);
+    return new Vector2(uCoord, vCoord);
+}
 
-            return color;
-        }
-        else
-        {
-            return new Color(0, 0, 0);
+
+    private static Color traceRay(Ray ray, Scene scene, BufferedImage texture) {
+    Intersection nearestIntersection = null;
+    Sphere nearestSphere = null;
+
+    for (Sphere s : scene.spheres) {
+        Intersection currentIntersection = RayTracer.intersectRaySphere(ray, s);
+        if (currentIntersection.hit && (nearestIntersection == null || currentIntersection.distance < nearestIntersection.distance)) {
+            nearestIntersection = currentIntersection;
+            nearestSphere = s;
         }
     }
+
+    if (nearestIntersection != null) {
+        // Calculate the UV coordinates on the sphere using the modified function name.
+        Vector2 uvCoordinates = SphereUV(nearestIntersection.point, nearestSphere);
+        // Sample the texture using the computed UV coordinates.
+        Color sampledColor = sampleTexture(texture, uvCoordinates);
+        return sampledColor;
+    } else {
+        return new Color(0, 0, 0);
+    }
+}
 
 
 
