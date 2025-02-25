db98cf9906067bba87f8f7ca2e0d1d5c14c776ad
Joshua Rivera <joshua.rivera1@calbaptist.edu>
2025-02-24 23:49:11 -0800

    finished init

--- OpenGLProjects/src/CubeSpinner.java
+++ OpenGLProjects/src/CubeSpinner.java
@@ -2,6 +2,7 @@
 //  java -XstartOnFirstThread \-Djava.library.path="lib/lwjgl-release-3.3.6-custom" \-classpath "lib/lwjgl-release-3.3.6-custom/*:src" \CelShading
 
 import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
 import org.lwjgl.system.MemoryStack;
 
@@ -19,6 +20,93 @@ import static org.lwjgl.system.MemoryUtil.*;
 
 
 public class CubeSpinner {
+    private long window;
+    private float angleX = 0.0f;
+    private float angleY = 0.0f;
+    private float speed = 0.1f;
+
+    public void run()
+    {
+        init();
+        loop();
+
+        // free the window callbacks and destroy the window
+        glfwFreeCallbacks(window);
+        glfwDestroyWindow(window);
+
+        // terminate GLFW and free the error callback
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+
+    private void init()
+    {
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        if ( !glfwInit() )
+            throw new IllegalStateException("Unable to initialize GLFW");
+
+        // Configure GLFW
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2); //3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1); //2);
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+        // create the window
+        window = glfwCreateWindow(300, 300, "Cube Spinner", NULL, NULL);
+        if ( window == NULL )
+            throw new RuntimeException("Failed to create the GLFW window");
+
+        // setup a key callback. It will be called every time a key is pressed, repeated, or released.
+        glfwSetKeyCallback(window, (window, key, scancode, action, mods) ->
+        {
+            if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+                glfwSetWindowShouldClose(window, true);
+        });
+
+        // Get the thread stack and push a new frame
+        try ( MemoryStack stack = stackPush() )
+        {
+            IntBuffer pWidth = stack.mallocInt(1);
+            IntBuffer pHeight = stack.mallocInt(1);
+
+            // get the window size passed to glfwCreateWindow
+            glfwGetWindowSize(window, pWidth, pHeight);
+
+            // get the resolution of the primary monitor
+            GLFWVidMode vidmode = glfwGetVideoMode(
+            glfwGetPrimaryMonitor());
+
+            // center the window
+            glfwSetWindowPos(
+                window,
+                    (vidmode.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            );
+        }
+
+        glfwMakeContextCurrent(window);
+        GL.createCapabilities();
+
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        gluPerspective(45.0f, (float) 300 / (float) 300, 0.1f, 100.0f);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+
+        int errorCode = glGetError();
+        if (errorCode != GL_NO_ERROR)
+            throw new RuntimeException("OpenGL error after context creation: " + errorCode);
+
+        glEnable(GL_DEPTH_TEST);
+        glDepthFunc(GL_LEQUAL);
+        glClearDepth(1.0f);
+        glfwSwapInterval(1);
+
+        // Make the window visible
+        glfwShowWindow(window);
+    }
 
     private void loop() {
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
@@ -32,7 +120,7 @@ public class CubeSpinner {
             glRotatef(angleY, 0.0f, 1.0f, 0.0f);
 
             drawCube();
-            glfwSwapBuffers(windows);
+            glfwSwapBuffers(window);
             glfwPollEvents();
         }
     }
