cbe0dbabdd9a841b07a6f02bc00f80c415e98fb2
devinnsegura <devinn.segura@calbaptist.edu>
2025-02-26 09:42:52 -0800

    Start for FluidSimulation.java

---
+++ OpenGLProjects/src/FluidSimulation.java
@@ -0,0 +1,64 @@
+public class FluidSimulation {
+    public static void main(String[] args) {
+
+    }
+
+    public void run() {
+
+    }
+    private void init() {
+
+    }
+
+    private void loop() {
+
+    }
+
+    private void updatePanTilt() {
+
+    }
+
+    private void renderPan() {
+
+    }
+
+
+
+
+}
+
+class Particle {
+
+    public Particle(float x, float y, float z) {
+
+    }
+
+    public void applyRepulsion(Particle other) {
+
+    }
+
+    public float cakePanPosition(float x, float z, float tX, float tZ) {
+
+    }
+
+    public void update(float tiltX, float tiltZ) {
+
+    }
+
+}
+
+class Water {
+    public Water(int count) {
+
+    }
+
+    public void update(float tiltX, float tiltZ) {
+
+    }
+
+    public void render() {
+
+    }
+}
+
+
